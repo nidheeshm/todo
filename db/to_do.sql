53accdac06d57ff9fe0e1d97d74a7ff90dfb47aa
nidheesh <nidheeshmohanant@gmail.com>
2018-12-09 10:04:32 +0530

    db file modification

--- db/to_do.sql
+++ db/to_do.sql
@@ -71,7 +71,6 @@ CREATE TABLE `users` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `role` int(10) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
